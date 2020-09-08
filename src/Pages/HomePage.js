7a87b8b95c4a8c66d5c81ec75113e1ebd5ae52a9
Olha Hrymchak <forwork1177@gmail.com>
2020-09-09 01:13:37 +0300

    added all yet

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './HomePage.module.css'
 const HomePage = () => {
   return (
-    <h2>HomePage</h2>
+    <>
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'/>
+<h1>
+  <em>P</em>
+  <em class="planet left">O</em>
+  <em>H</em>
+  <em>O</em>
+  <em class="planet right">O</em>
+  <em>N</em>
+  <em>E</em>
+  <em>B</em>
+  <em>O</em>
+  <em>O</em>
+  <em>K</em>
+</h1>
+
+</>
   )
 };
 export default HomePage
