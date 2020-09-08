bd042c9a467bc075231483f8d4737d6ba472436c
Olha Hrymchak <forwork1177@gmail.com>
2020-09-09 00:53:16 +0300

    all

--- src/Components/UserMenu/UserMenu.js
+++ src/Components/UserMenu/UserMenu.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import authSelectors from '../../Redux/Selections/authSelector'
 import authOperations from  '../../Redux/Operations/authOperations'
+import img from "../../image/88841e3ffd244bb0c9c37ce05256e446.gif"
+import css from "./UserMenu.module.css"
 
 
 
 const UserMenu = ({ avatar, name, onLogoutUser }) => (
   <div >
-    <img src={avatar} alt="" width="32" />
+    <img src={avatar} alt="" width="32"  className={css.userMenu_img}/>
     <span >Welcome, {name}</span>
-    <button type="button" onClick={onLogoutUser}>
+    <button type="button" onClick={onLogoutUser} className={css.userMenu_Button}>
       Logout
     </button>
   </div>
@@ -17,8 +19,7 @@ const UserMenu = ({ avatar, name, onLogoutUser }) => (
 
 const mapStateToProps = state => ({
   name: authSelectors.getUserName(state),
-  avatar:
-    'https://icon-library.net/images/avatar-icon-images/avatar-icon-images-7.jpg',
+  avatar: `${img}`,
 });
 
 export default connect(mapStateToProps, { onLogoutUser: authOperations.logOut })(UserMenu);
